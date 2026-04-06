53ac0fa3f2e12791da86c5c03d05158513e310b2
Johnrosa59 <jvrosatecnologia@gmail.com>
2026-04-06 14:12:43 -0300

    Mais alterações

--- app/src/main/java/com/joaorosa/climatempoproject/RegisterActivity.kt
+++ app/src/main/java/com/joaorosa/climatempoproject/RegisterActivity.kt
@@ -1,10 +1,12 @@
 package com.joaorosa.climatempoproject
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 import com.joaorosa.climatempoproject.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
@@ -13,9 +15,48 @@ class RegisterActivity : AppCompatActivity() {
         ActivityRegisterBinding.inflate(layoutInflater)
     }
 
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        binding.btnSendCadastre.setOnClickListener {
+            registerUser()
+            registerSex()
+        }
+
+    }
+
+    fun registerUser() {
+        val emailRegistered = binding.editTextEmailRegister.text.toString()
+
+        if(binding.editTextPasswordRegister.text.toString() == binding.editTextPasswordConfirm.text.toString()){
+            val passwordRegistered = binding.editTextPasswordConfirm.text.toString()
+    }else{
+        showMessage("Senhas não conferem")
+        }
+}
+
+
+    fun showMessage(mensagem: String){
+        Toast.makeText(
+            this, mensagem,
+            Toast.LENGTH_LONG)
+            .show()
+    }
+
+    fun registerSex() {
+        val selectedMale = binding.radioBtnMale.isChecked
+        val selectedFemale = binding.radioBtnFemale.isChecked
+        if(selectedMale){
+
+        }else if (selectedFemale){
+
+        }
     }
 }
